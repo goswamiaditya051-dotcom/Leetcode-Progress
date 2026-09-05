54ee43817a874212b148164ab5b45d707d740a50
Aditya <goswamiaditya051@gmail.com>
2026-09-05 17:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (77.91%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,29 +1,27 @@
 class Solution {
     public double myPow(double a, long b) {
         if(b==0) return 1;
-        // boolean flag = true;
         if(b<0)
         {
-            double call  = myPow(a,Math.abs(b)/2);
-
+            b = Math.abs(b);
+            double call1 = myPow( a, b/2);
             if(b%2==0)
             {
-                double ans = 1/(call * call);
-                return ans;
+                return 1/(call1 * call1);
             }
-            else{
-                double ans = 1/(call * call * a);
-                return ans;
+            else
+            {
+                return 1/(call1 * call1*a);
             }
         }
         else{
-            double call = myPow(a,(long)b/2);
-            if(b%2==0) {
-                
+            double call = myPow(a,b/2);
+            if(b%2==0)
+            {
                 return call * call;
             }
-            else {
-                return call * call *a;
+            else{
+                return call * call * a;
             }
         }
     }
