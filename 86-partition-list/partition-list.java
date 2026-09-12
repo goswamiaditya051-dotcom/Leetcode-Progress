bf60851c422a1f128b6fbac2077d5b107e92b5d4
Aditya <goswamiaditya051@gmail.com>
2026-09-12 14:05:45 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (13.53%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -10,32 +10,31 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-
         ListNode d1 = new ListNode(-1);
         ListNode d2 = new ListNode(-1);
-        ListNode a = d1;
-        ListNode b = d2;
+
+        ListNode c = d1;
+        ListNode d = d2;
+
         ListNode i = head;
-        while (i != null) 
+        while(i!=null)
         {
             if(i.val<x)
             {
-                a.next = i;
-                a = a.next;
+                c.next = i;
+                c = c.next;
                 i = i.next;
             }
             else if(i.val>=x)
             {
-                b.next = i;
-                b = b.next;
+                d.next = i;
+                d = d.next;
                 i = i.next;
             }
+            
         }
-       
-        b.next = null;
-        a.next = d2.next;
-
+        d.next = null;
+        c.next = d2.next;
         return d1.next;
-
     }
 }
