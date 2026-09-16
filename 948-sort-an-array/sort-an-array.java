2f4541a24f5f33592610da7833b1f3dd035e6474
Aditya <goswamiaditya051@gmail.com>
2026-09-16 18:59:56 +0530

    Time: 29 ms (32.51%) | Memory: 82.5 MB (80.37%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,26 +1,34 @@
 class Solution {
     public int[] sortArray(int[] arr) {
-        int n = arr.length;
-        if(n==1) return arr;
-        int[]brr = new int[n/2];
-        int[]crr = new int[n-n/2];
-        int idx = 0;
-        for(int i = 0; i<brr.length; i++)
+        
+       int n = arr.length;
+       if(n==1) return arr;
+
+       int[]brr = new int[n/2];
+       int[]crr = new int[n-n/2];
+
+       int idx = 0;
+
+       for(int i = 0; i<brr.length; i++)
         {
             brr[i] = arr[idx++];
         }
+
         for(int j = 0; j<crr.length; j++)
         {
             crr[j] = arr[idx++];
         }
+
         sortArray(brr);
         sortArray(crr);
         merge(brr,crr,arr);
         return arr;
-
     }
-    public static int []merge(int[]left,int[]right,int[]crr){
-        int i = 0; int j = 0; int k = 0;
+    public static int[]merge(int[]left,int[]right,int[]crr )
+    {
+        int i = 0;
+        int j = 0;
+        int k = 0;
 
         while(i<left.length && j<right.length)
         {
@@ -35,14 +43,12 @@ class Solution {
             }
             k++;
         }
-        while(i<left.length)
-        {
+        while(i<left.length){
             crr[k] = left[i];
             i++;
             k++;
         }
-        while(j<right.length)
-        {
+        while(j<right.length){
             crr[k] = right[j];
             j++;
             k++;
