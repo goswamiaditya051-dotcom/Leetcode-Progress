4a86b499c608fcc4a6d62c44fd1ea24d3ac175a8
Aditya <goswamiaditya051@gmail.com>
2026-09-24 14:53:45 +0530

    Time: 72 ms (26.93%) | Memory: 134.8 MB (25.71%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -9,38 +9,38 @@
  * }
  */
 class Solution {
-     public ListNode removeNodes(ListNode head) {
-    Stack<ListNode>st = new Stack<>();
-    ListNode temp = head;
-    while (temp!=null){
+    public ListNode removeNodes(ListNode head) {
+       ListNode temp = head;
+       Stack<ListNode> st = new Stack<>();
 
-        while(st.size()>0 && st.peek().val < temp.val )
+       while(temp!=null)
+       {
+            while(st.size()>0 && st.peek().val < temp.val)
+            {
+                st.pop();
+            }
+            st.push(temp);
+            temp = temp.next;
+       }
+
+        ListNode newHead = st.pop();
+        temp = newHead;
+        while(st.size()>0)
         {
-            st.pop();
+            ListNode top = st.pop();
+            temp.next = top;
+            temp = top;
         }
-        st.push(temp);
-        temp = temp.next;
-    }
-    ListNode newHead = st.pop();
-    temp = newHead;
-    ListNode top = null;
-    while (st.size()>0){
-         top = st.pop();
-        temp.next = top;
-        temp = top;
-    }
-    // Now we will reverse the linkedList
         temp.next = null;
+
         return reverse(newHead);
-
-
+        
     }
-
-    private ListNode reverse(ListNode newHead) {
+    public static ListNode reverse(ListNode head)
+    {
+        ListNode curr = head;
         ListNode prev = null;
         ListNode fwd = null;
-        ListNode curr = newHead;
-
         while(curr!=null)
         {
             fwd = curr.next;
